aad069f0ee99ecaa3a190dd9e204ec7abaf823b4
ouwargui <guikemon2@gmail.com>
2022-07-25 23:34:50 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import 'express-async-errors';
 import express from 'express';
 import {routes} from './routes';
+import {ErrorHandler} from './middlewares/ErrorHandler';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1', routes);
 
+app.use('/api/v1', routes);
 app.get('/', (req, res) => res.status(200).send('Api is running'));
+
+app.use(ErrorHandler);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
