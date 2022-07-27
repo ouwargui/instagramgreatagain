c9f934c47dabb1c1748f8a6e157651e29c395b2f
ouwargui <guikemon2@gmail.com>
2022-07-27 01:11:15 -0300

    refactor: move file uploader to usecase instead of controller

--- src/modules/post/useCases/createPost/CreatePostController.ts
+++ src/modules/post/useCases/createPost/CreatePostController.ts
@@ -1,31 +1,21 @@
 import {Request, Response} from 'express';
-import {
-  File,
-  IFileUploader,
-} from '../../../../infra/fileUploader/IFileUploader';
+import {File} from '../../../../infra/fileUploader/IFileUploader';
 import CreatePostUseCase from './CreatePostUseCase';
 
 class CreatePostController {
   private createPostUseCase: CreatePostUseCase;
-  private fileUploader: IFileUploader;
 
-  constructor(
-    createPostUseCase: CreatePostUseCase,
-    fileUploader: IFileUploader,
-  ) {
+  constructor(createPostUseCase: CreatePostUseCase) {
     this.createPostUseCase = createPostUseCase;
-    this.fileUploader = fileUploader;
   }
 
   async handle(req: Request, res: Response): Promise<Response> {
     const {author_id, description} = req.body;
     const {files} = req.body as {files: File[]};
 
-    const files_path = await this.fileUploader.upload(files);
-
     const post = await this.createPostUseCase.execute({
       author_id,
-      pics: files_path ?? [],
+      files,
       description,
     });
 
--- src/modules/post/useCases/createPost/CreatePostUseCase.ts
+++ src/modules/post/useCases/createPost/CreatePostUseCase.ts
@@ -1,22 +1,34 @@
+import {
+  File,
+  IFileUploader,
+} from '../../../../infra/fileUploader/IFileUploader';
 import {Post} from '../../model/Post';
 import {IPostsRepository} from '../../repositories/IPostsRepository';
 
 interface ICreatePost {
   author_id: number;
-  pics: string[];
+  files: File[];
   description: string;
 }
 
 class CreatePostUseCase {
   private postsRepository: IPostsRepository;
+  private fileUploader: IFileUploader;
 
-  constructor(postsRepository: IPostsRepository) {
+  constructor(postsRepository: IPostsRepository, fileUploader: IFileUploader) {
     this.postsRepository = postsRepository;
+    this.fileUploader = fileUploader;
   }
 
-  async execute({author_id, pics, description}: ICreatePost): Promise<Post> {
-    if (!author_id || !pics || pics.length < 1 || !description) {
+  async execute({author_id, files, description}: ICreatePost): Promise<Post> {
+    if (!author_id || !files || !description) {
       throw new Error('Missing required fields');
+    }
+
+    const pics = await this.fileUploader.upload(files);
+
+    if (!pics) {
+      throw new Error('Error uploading files');
     }
 
     return this.postsRepository.create({author_id, pics, description});
